99fdf25231ad98769ac09be81f7f1428be885837
Kevklatman <116696816+Kevklatman@users.noreply.github.com>
2024-09-04 15:15:48 -0600

    basic setup

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,12 +1,39 @@
 class Article:
+    all = []
+
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
-        self.title = title
+        if isinstance(title, str) and 5 <= len(title) <= 50  :
+            self._title = title
+        else:
+            raise TypeError(
+                "Title must be a type of string."
+            )
+
+    @property
+    def title(self):
+        if hasattr(self, '_title'):
+            return self._title
+        else: 
+            raise AttributeError("title has not been initialized")
+    
+
         
 class Author:
     def __init__(self, name):
-        self.name = name
+        if isinstance(name, str) and len(name) > 0:
+            self._name = name
+        else:
+            raise ValueError("Author name must be a non-empty string.")
+
+    @property
+    def name(self):
+        if hasattr(self, '_name'):
+            return self._name
+        else:
+            raise AttributeError("Author name has not been initialized.")
+        
 
     def articles(self):
         pass
